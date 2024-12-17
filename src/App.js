d3db6cd238f103c9d8d9f52abfd2d42698cf5304
Pedro H. Silva <impedrohenri@gmail.com>
2024-12-17 00:45:37 -0300

    componentiza a requisição da API

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/header/Header.js';
-import MsgErro from './components/msgErro/MsgErro.js';
-import ListaProdutos from './components/listaProdutos/ListaProdutos.js';
-import MsgCarregando from './components/msgCarregando/MsgCarregando.js';
+import ListaProdutos from './components/listaProdutos/ListaProdutos.js'
 
 function App() {
-  const [respAPI, setRespAPI] = useState([])
-  const [respStatus, setRespStatus] = useState()
-  const [erro, setErro] = useState(null)
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then(res => {
-        setRespStatus(res.status)
-        return res.json()
-      }
-      )
-      .then(json => {
-        setRespAPI(json)
-      })
-      .catch(error => setErro(error));
-  }, [])
-
-
+  
   return (
     <>
-      <Header />
-      {
-      erro ? 
-      (<MsgErro erroStatus={respStatus} />) : 
-      respAPI.length === 0 ? 
-      (<MsgCarregando />) : 
-      (<ListaProdutos produtos={respAPI} />)
-      }
+      <Header/>
+      <ListaProdutos/>
     </>
   );
 }
--- src/components/listaProdutos/ListaProdutos.js
+++ src/components/listaProdutos/ListaProdutos.js
@@ -1,12 +1,42 @@
+import { useEffect, useState } from 'react';
 import './ListaProdutos.css'
-import Card from '../card/Card'
+import Card from '../card/Card.js'
+import MsgErro from '../msgErro/MsgErro.js';
+import MsgCarregando from '../msgCarregando/MsgCarregando.js';
 
-export default function ListaProdutos({produtos}) {
+
+export default function ListaProdutos() {
+
+    const [respAPI, setRespAPI] = useState([])
+    const [respStatus, setRespStatus] = useState()
+    const [erro, setErro] = useState(null)
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then(res => {
+            setRespStatus(res.status)
+            return res.json()
+        }
+        )
+        .then(json => {
+            setRespAPI(json)
+        })
+        .catch(error => setErro(error));
+    }, [])
+
     return (
-    <>
-        <div className='lista d-flex flex-wrap'>
-            {produtos.map(produto => <Card key={produto.id} item={produto} />)}
-        </div>
-    </>
+        <>
+            {
+            erro ? 
+            (<MsgErro erroStatus={respStatus} />) : 
+            respAPI.length === 0 ? 
+            (<MsgCarregando />) : 
+            (<>
+                <div className='lista d-flex flex-wrap'>
+                    {respAPI.map(produto => <Card key={produto.id} item={produto} />)}
+                </div>
+            </>)
+            }
+        </>
     )
 }
